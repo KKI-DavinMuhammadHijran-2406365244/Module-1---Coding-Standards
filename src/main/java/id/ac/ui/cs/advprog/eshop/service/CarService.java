ba798c60b6c6a13924843624b1aac04fac68a328
Davin Muhammad Hijran <davinmagic3@gmail.com>
2026-02-28 15:40:01 +0700

    Creating car service and serviceImpl

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarService.java
@@ -0,0 +1,17 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import java.util.List;
+
+public interface CarService {
+
+    Car create(Car car);
+
+    List<Car> findAll();
+
+    Car findById(String carId);
+
+    void update(String carId Car car);
+
+    void deleteCarbyId(String carId);
+}
