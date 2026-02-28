1af2756a1f058b097d21fb6c9e0eca76dcbf1f6b
Davin Muhammad Hijran <davinmagic3@gmail.com>
2026-02-28 22:05:34 +0700

    finishing solid code

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
@@ -1,58 +1,16 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
 import id.ac.ui.cs.advprog.eshop.model.Car;
-import org.springframework.stereotype.Repository;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.UUID;
 
-@Repository
-public class CarRepository {
-    private static int id = 0;
-    private final List<Car> carData = new ArrayList<>();
+public interface CarRepository {
+    Car create(Car car);
 
-    // CREATE
-    public Car create(Car car) {
-        if (car.getCarId() == null) {
-            UUID uuid = UUID.randomUUID();
-            car.setCarId(uuid.toString());
-        }
-        carData.add(car);
-        return car;
-    }
+    List<Car> findAll();
 
-    // READ ALL
-    public Iterator<Car> findAll() {
-        return carData.iterator();
-    }
+    Car findById(String id);
 
-    // READ BY ID
-    public Car findById(String id) {
-        for (Car car : carData) {
-            if (car.getCarId().equals(id)) {
-                return car;
-            }
-        }
-        return null;
-    }
+    Car update(String id, Car updatedCar);
 
-    // UPDATE
-    public Car update(String id, Car updatedCar) {
-        for (int i = 0; i < carData.size(); i++) {
-            Car car = carData.get(i);
-            if (car.getCarId().equals(id)) {
-                car.setCarName(updatedCar.getCarName());
-                car.setCarColor(updatedCar.getCarColor());
-                car.setCarQuantity(updatedCar.getCarQuantity());
-                return car;
-            }
-        }
-        return null; // Car not found
-    }
-
-    // DELETE
-    public void delete(String id) {
-        carData.removeIf(car -> car.getCarId().equals(id));
-    }
+    void delete(String id);
 }
